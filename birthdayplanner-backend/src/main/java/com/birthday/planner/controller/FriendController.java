8fcdfbbe63c84b10ec0838427b6f4d471b89ae2d
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2021-02-15 15:41:40 -0500

    Creating Add Friend REST API

--- birthdayplanner-backend/src/main/java/com/birthday/planner/controller/FriendController.java
+++ birthdayplanner-backend/src/main/java/com/birthday/planner/controller/FriendController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,11 @@ public class FriendController {
 	@GetMapping("/friends")
 	public List<Friends> getAllFriends() {
 		return friendRepository.findAll();
+	}
+	
+	// Create friend REST API
+	@PostMapping("/friends")
+	public Friends createFriend(@RequestBody Friends friend) {
+		return friendRepository.save(friend);
 	}
 }
