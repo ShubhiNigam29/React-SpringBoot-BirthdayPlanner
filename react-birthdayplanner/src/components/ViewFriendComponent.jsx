f5911d7a25c11135dcbbf942fbc46891572fa7d9
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2021-02-16 03:38:19 -0500

    Show Data on View Friend Page

--- react-birthdayplanner/src/components/ViewFriendComponent.jsx
+++ react-birthdayplanner/src/components/ViewFriendComponent.jsx
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
+import FriendService from '../services/FriendService';
 
 class ViewFriendComponent extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            id: this.props.match.params.id
+            id: this.props.match.params.id,
+            friend: {}
         }
+    }
+
+    componentDidMount() {
+        FriendService.getFriendById(this.state.id).then((res) => {
+            this.setState({friend: res.data});
+        });
     }
 
     render() {
         return (
             <div>
-                
+                <br></br>
+                <div className="card col-md-6 offset-md-3">
+                    <h3 className="text-center">View Friend Details</h3>
+                    <div className="card-body">
+                        <div className="row">
+                            <label> Friend First Name: </label>
+                            <div>{this.state.friend.firstName}</div>
+                        </div>
+                        <div className="row">
+                            <label> Friend Last Name: </label>
+                            <div>{this.state.friend.lastName}</div>
+                        </div>
+                        <div className="row">
+                            <label> Friend Birth Date: </label>
+                            <div>{this.state.friend.birthDate}</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
