087d70eb4b289823bedf1b0642d0da9a6c06c438
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2021-02-15 21:08:07 -0500

    Creating Update Friend REST API

--- birthdayplanner-backend/src/main/java/com/birthday/planner/controller/FriendController.java
+++ birthdayplanner-backend/src/main/java/com/birthday/planner/controller/FriendController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,16 @@ public class FriendController {
 	public ResponseEntity<Friends> getFriendById(@PathVariable Long id) {
 		Friends friend = friendRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Friend doesn't exist with id: "+id));
 		return ResponseEntity.ok(friend);
+	}
+	
+	// Update Friend REST API
+	@PutMapping("/friends/{id}")
+	public ResponseEntity<Friends> updateFriend(@PathVariable Long id, @RequestBody Friends friendDetails) {
+		Friends friend = friendRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Friend doesn't exist with id: "+id));
+		friend.setFirstName(friendDetails.getFirstName());
+		friend.setLastName(friendDetails.getLastName());
+		friend.setBirthDate(friendDetails.getBirthDate());
+		Friends updatedFriend = friendRepository.save(friend);
+		return ResponseEntity.ok(updatedFriend);
 	}
 }
